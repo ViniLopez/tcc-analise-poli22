9b6c28080359672853ccd613c70bbb8de91e1b3d
Vinicius Lopez <83730084+ViniLopez@users.noreply.github.com>
2022-11-15 11:04:47 -0300

    Update and rename about.py to 99_about.py

---
+++ pages/99_about.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.markdown("# About")
+st.sidebar.markdown("# About")
+
+st.title("Sobre o projeto")
+st.subheader("Trabalho de conclusão de curso da Aline, Camila e Vinícius na Escola Politécnica da USP")
--- pages/about.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Sobre o projeto")
-st.subheader("bla bla")

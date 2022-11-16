c12d5b74e5dfe12dc58024dcafe22db1666cbb3a
Vinicius Lopez <83730084+ViniLopez@users.noreply.github.com>
2022-11-15 23:45:08 -0300

    Update 01_avaliar_empresa.py

--- pages/01_avaliar_empresa.py
+++ pages/01_avaliar_empresa.py
@@ -1,4 +1,27 @@
 import streamlit as st
 
 st.title("Tese carregada - Análise de empresa")
-st.subheader("Preencha os dados a seguir para avaliar essa empresa e sua aderência à tese de " + nome)
+
+# Camila - GET nome do investidor 
+# Camila - GET variáveis X e Y da tese, pré-processada na home
+
+with st.form("Preencha os dados a seguir para avaliar a aderência da empresaà tese:", clear_on_submit=False):
+    nome_founder =  st.text_input("Seu nome:", placeholder="Fulano da Silva")
+    email_founder = st.text_input("Email:", placeholder="fulano.silva@gmail.com")
+    telefone_founder = st.text_input("Telefone:", placeholder="(__) _____-____")
+    nome_empresa =  st.text_input("Nome da sua empresa:", placeholder="Nubank")
+    data_fundacao = st.date_input("Data da fundação:", max_value=datetime.date.today())
+    data_submissao = datetime.date.today()
+    qtd_funcionarios = st.number_input("Quantidade de funcionários:", min_value=1, value=1, step=1, format='%d')
+    industria = st.text_input("À qual categoria sua indústria pertence?", placeholder="Cleantech")
+    prod_proprio = st.radio('Seu produto principal é próprio?', ['Sim', 'Não'])
+    
+    # Camila - POST das infos da empresa acima
+    
+    submit = st.form_submit_button("Fazer cadastro")                                 
+  if submit:
+    st.success('')
+    with st.spinner('Analisando sua tese...'):
+      # Modulos: 6 e 7 do colab
+      st.write(tese.head())
+      time.sleep(2)

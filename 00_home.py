8bb9a21e0cbc068aad0425b8ea0b169528d3ef1e
Vinicius Lopez <83730084+ViniLopez@users.noreply.github.com>
2022-11-15 23:34:15 -0300

    Update 00_home.py

--- 00_home.py
+++ 00_home.py
@@ -76,7 +76,7 @@ elif perfil == 'Empreendedor':
     nome_founder =  st.text_input("Seu nome:", placeholder="Fulano da Silva")
     email_founder = st.text_input("Email:", placeholder="fulano.silva@gmail.com")
     telefone_founder = st.text_input("Telefone:", placeholder="(__) _____-____")
-    nome_founder =  st.text_input("Nome da sua empresa:", placeholder="Nubank")
+    nome_empresa =  st.text_input("Nome da sua empresa:", placeholder="Nubank")
     data_fundacao = st.date_input("Data da fundação:", max_value=datetime.date.today())
     data_submissao = datetime.date.today()
     qtd_funcionarios = st.number_input("Quantidade de funcionários:", min_value=1, value=1, step=1, format='%d')

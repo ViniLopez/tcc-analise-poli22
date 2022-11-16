d2c7d75ec1245643d7cee9fc3e3e86452474d6c5
Vinicius Lopez <83730084+ViniLopez@users.noreply.github.com>
2022-11-15 22:19:52 -0300

    Update 01_avaliar_empresa.py

--- pages/01_avaliar_empresa.py
+++ pages/01_avaliar_empresa.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title("Tese carregada - Análise de empresa")
-st.subheader("Preencha os dados a seguir para avaliar essa empresa e sua aderência à tese de " + nome)
+#st.subheader("Preencha os dados a seguir para avaliar essa empresa e sua aderência à tese de " + nome)

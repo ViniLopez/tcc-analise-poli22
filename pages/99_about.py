533a17f44f14d9d20d4beb618f402ada02b17461
Vinicius Lopez <83730084+ViniLopez@users.noreply.github.com>
2022-11-22 17:54:40 -0300

    Update 99_about.py

--- pages/99_about.py
+++ pages/99_about.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
 st.title("Sobre o projeto")
-st.subheader("Trabalho de conclusão de curso da Aline, Camila e Vinícius na Escola Politécnica da USP")
+st.subheader("Trabalho de conclusão de curso da Aline Tsuruda, Camila Miwa e Vinícius Lopez na Escola Politécnica da USP - 2022")
+
+st.markdown("--")
+st.write("Este projeto tem o intuito de aplicar os conceitos estudados ao longo do curso de Engenharia Elétrica, ênfase em Computação para a criação de uma ferramenta de suporte à decisão de investimentos.")

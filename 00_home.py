a078048749ebaf2d528364ea01fcafccb7e363a5
Vinicius Lopez <83730084+ViniLopez@users.noreply.github.com>
2022-11-22 16:18:35 -0300

    Update 00_home.py

--- 00_home.py
+++ 00_home.py
@@ -7,6 +7,7 @@ from pymongo import MongoClient
 st.title("TCC - AUTOMATIZAÇÃO DE ANÁLISE DE EMPRESAS PARA AUXÍLIO DE DECISÃO DE INVESTIMENTOS")
 st.write("Ferramenta de suporte para decisão de investimento em startups a partir de Machine Learning")
 
+"""
 ###################################################################################
 # Initialize connection.
 # Uses st.experimental_singleton to only run once.
@@ -30,6 +31,7 @@ st.write(f"{message['name']} has a {message['email']}")
 #for item in items:
 #    st.write(f"{item['name']} has a {item['email']}")
 ###################################################################################
+"""
 
 st.subheader("Bem-vindo ao projeto, primeiramente nos diga, quem é você:")
 perfil = st.selectbox('Eu sou:', ['Investidor', 'Empreendedor'])
@@ -71,8 +73,8 @@ elif perfil == 'Empreendedor':
 st.markdown("""---""")
 st.subheader("Propósito do projeto:")
 st.write("Temos a visão de nos tornarmos um canal de conexão entre Investidores e Empreendedores. Através desta plataforma, é possível encontrar potenciais novas parcerias! Conheça mais na página a seguir:")
-#  if(st.button("Conhecer saber mais do projeto!")):
-#    choice = 'About'
+if(st.button("Conhecer saber mais do projeto!")):
+    choice = 'About'
 
 """
 Snippet para mudar de pagina com o metodo switch_page:

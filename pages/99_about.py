ff8300ea0227226a423533b68d47c88ee9213007
Vinicius Lopez <83730084+ViniLopez@users.noreply.github.com>
2022-11-23 07:26:11 -0300

    Update 99_about.py

--- pages/99_about.py
+++ pages/99_about.py
@@ -12,13 +12,17 @@ with col2:
     st.write("Orientados por:\n  - Prof. Dr. Reginaldo Arakaki\n  - Co-Orientador: Victor Takashi Hayashi")
 
 st.markdown("""---""")
+st.subheader("Objetivo")
+
 st.write("Este projeto tem o intuito de aplicar os conceitos estudados ao longo do curso de Engenharia Elétrica, ênfase em Computação para a criação de uma ferramenta de suporte à decisão de investimentos.")
 
 st.write("A ferramenta, recebe dos investidores uma tese de investimentos implícita através de um histórico de empresas que já foram previamente analisadas. A partir desses exemplos, faz um tratamento de dados prévio (incluindo rotinas de Data Augmentation e Engenharia de Variáveis) e treina dois algoritmos de Machine Learning Supervisionados para classificação: K-Nearest Neighbours e Random Forest.")
 st.write("Em seguida, recebe a descrição de uma empresa candidata para avaliar se a mesma está aderente às teses cadastradas na plataforma.")
 
 st.markdown("""---""")
+st.subheader("Trabalho completo")
 st.write("Para conhecer mais do projeto, acesse o relatório completo do trabalho disponível em [Relatório TCC](https://docs.google.com/document/d/1jcARZxQTIwcTSX0b9vaBgocrXZKcrAfRFKzAaypXa74/edit?usp=sharing) ou o repositório do projeto final [Github - TCC](https://github.com/ViniLopez/tcc-analise-poli22).")
 
 st.markdown("""---""")
-st.write("Contatos:\n  - Aline Lorena Tsuruda: [LinkedIn](https://www.linkedin.com/in/aline-tsuruda/)\n  - Camila Miwa Ivano: [LinkedIn](https://www.linkedin.com/in/camila-miwa-ivano/)\n  - Vinícius Cardieri Lopez: [LinkedIn](https://www.linkedin.com/in/vinicius-cardieri-lopez-79ba17163/)")
+st.subheader("Contatos")
+st.write("  - Aline Lorena Tsuruda: [LinkedIn](https://www.linkedin.com/in/aline-tsuruda/)\n  - Camila Miwa Ivano: [LinkedIn](https://www.linkedin.com/in/camila-miwa-ivano/)\n  - Vinícius Cardieri Lopez: [LinkedIn](https://www.linkedin.com/in/vinicius-cardieri-lopez-79ba17163/)")

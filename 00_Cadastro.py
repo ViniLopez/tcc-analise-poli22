113aeab75d4db0fd403927fea5d4b91cac104edc
Vinicius Lopez <83730084+ViniLopez@users.noreply.github.com>
2022-11-23 07:47:01 -0300

    Update 00_Cadastro.py

--- 00_Cadastro.py
+++ 00_Cadastro.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-# from streamlit_extras.switch_page_button import switch_page
+from streamlit_extras.switch_page_button import switch_page
 import datetime
 import time
 

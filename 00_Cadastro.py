893e815f51344a17f61fb6c4c8a7a35a6f394b8b
Vinicius Lopez <83730084+ViniLopez@users.noreply.github.com>
2022-11-23 17:39:49 -0300

    Update 00_Cadastro.py

--- 00_Cadastro.py
+++ 00_Cadastro.py
@@ -48,7 +48,7 @@ if perfil == 'Investidor':
     if isApiRunning:
       # Baixando template disponível no repositório
       pandas_load = pd.read_json("investor_empty.json")
-      guia_importacao = pandas_load.to_csv(index = False, encoding='iso-8859-1')
+      guia_importacao = pandas_load.to_csv(index = False, encoding='latin1')
 
       baixou_modelo = st.download_button('Download modelo',
                                         data=guia_importacao,
@@ -68,7 +68,7 @@ if perfil == 'Investidor':
     if isApiRunning:
       pandas_load = pd.read_csv("Tese_Investimento_Exemplo.csv")
       st.write(pandas_load.head())
-      exemplo_tese = pandas_load.to_csv(index = False, encoding='iso-8859-1')
+      exemplo_tese = pandas_load.to_csv(index = False, encoding='latin1')
 
       baixou_modelo = st.download_button('Download modelo',
                                         data=exemplo_tese,
